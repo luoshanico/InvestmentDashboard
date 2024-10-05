7ce967fd9fe0e1602548646d53a0eb43c601cd3e
“Nico <nicoworrall55@gmail.com>
2024-10-05 14:11:01 +0800

    Total portfolio valuation graph added

--- dashboard_page.py
+++ dashboard_page.py
@@ -7,15 +7,37 @@ import calculations as calcs
 
 def show_dashboard_page(conn):
 
+    # DataFrame with key valuation and holding data
+    df_values = calcs.get_holdings_values(conn)
+    
     # Title
     st.title("Investment Dashboard")
 
-    # Valuation graph
-    st.subheader("Valuation")
+    # Total value graph
+    st.write("Total portfolio value:")
     
-    df_values = calcs.get_holdings_values(conn)
-    df_assets = calcs.get_unit_holdings(conn)
+    df_total_value = df_values[['Date','Value']].groupby(['Date']).sum()
+        
+    # Graph
+    fig, ax  = plt.subplots(figsize=(10,6))
+    ax.plot(df_total_value.index, df_total_value['Value'], label='Portfolio value')  # Plot portfolio value by date
+    
+    # Add labels and title
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Value')
+    ax.set_title('Asset Holdings Over Time')
+    ax.legend(title='Asset')
 
+    # Rotate x-axis labels for better readability
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    # Display the Matplotlib figure using st.pyplot()
+    st.pyplot(fig)
+
+    # Graph of value by holding
+    st.write("Value by holding:")
+    
     # Create a plot for each asset
     fig, ax  = plt.subplots(figsize=(10,6))
 
